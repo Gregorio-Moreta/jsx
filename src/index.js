79173c6cdc168a55ee87556f763fe64a42221457
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2023-03-14 18:52:28 -0400

    understanding how datatypes are managed with react jsx

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // 1 import react and ReactDOM libraries
-
 // Library that defines what a component is and how multiple components work together
 import React from 'react'
 // Library that knows how to get a component to show up in the brtwoser
@@ -16,7 +15,22 @@ const root = ReactDOM.createRoot(el)
 
 // 4 Create a component
 function APP() {
-    return <h1>hello world</h1>
+    let message = 'Bye, there!'
+    if (Math.random() > 0.5) {
+        message = 'Hello there!'
+    }
+// Most common to use numbers and strings for values stored in variabls for JSX variables
+// Objects create error messages and are not allowed as react chlldren
+// React treats bookleans, nulls, and arrays in unexpected ways
+//  Arrays don't have commas or [] prints each item independently
+    const messageTwo = 123
+
+    return (
+        <div>
+            <h1>{message}</h1>
+            <h1>{messageTwo}</h1>
+        </div>
+    )
 }
 
 
